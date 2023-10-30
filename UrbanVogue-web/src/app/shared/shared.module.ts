7bde246dc9e24a6628d2b510d1cfa066322e875c
vanyavidivici <vanyavidivici@gmail.com>
2023-10-31 01:59:23 +0200

    added based layout for header, footer and carousel on main page components

--- UrbanVogue-web/src/app/shared/shared.module.ts
+++ UrbanVogue-web/src/app/shared/shared.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MainCarouselComponent } from './components/main-carousel/main-carousel.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MainCarouselComponent
+  ],
+  exports: [
+    MainCarouselComponent
+  ],
   imports: [
     CommonModule
   ]
